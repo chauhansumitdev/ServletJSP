fd59ee70d5470ed40e9569776b9a5d99b9470bad
Sumit Chauhan <chauhansumit9009@gmail.com>
2024-05-30 13:09:35 +0530

    init

---
+++ src/main/java/com/example/Hello.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Hello extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.println("<strong>class hit << Hello.java</strong>");
+        }
+
+        System.out.println("class hit");
+    }
+}
